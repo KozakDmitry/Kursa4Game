4f70e579acbf25e78061e2d353c0a256944f3cc1
Admin <dimakaza4ka@gmail.com>
2020-05-04 17:18:12 +0300

    First try

---
+++ Assets/Scripts/GameScripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public float levelStartDelay = 2f;                      //Time to wait before starting level, in seconds.
+	public static GameManager instance = null;
+	[HideInInspector] public bool playersTurn = true;       //Boolean to check if it's players turn, hidden in inspector but public.
+
+	private GameObject levelImage;                         
+	private LevelManager levelManager;
+	private bool doingSetup = true;
+
+
+
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+		DontDestroyOnLoad(gameObject);
+		levelManager = GetComponent<LevelManager>();
+		InitGame();
+	}
+	void InitGame()
+	{
+		doingSetup = true;
+		levelImage = GameObject.Find("LevelImage");
+	}
+
+	
+}
---
+++ Assets/Scripts/GameScripts/Loader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Loader : MonoBehaviour
+{
+	public GameObject gameManager;
+	//public GameObject soundManager;        
+	public Button button;
+
+	void Awake()
+	{
+	
+		if (GameManager.instance == null)
+			Instantiate(gameManager);
+
+		
+		//if (SoundManager.instance == null)
+
+		//	//Instantiate SoundManager prefab
+		//	Instantiate(soundManager);
+	}
+
+	void Start()
+	{
+
+	}
+
+
+	public void StartGameButton()
+	{
+		SceneManager.LoadScene("Test");
+	}
+
+
+	public void OptionsButton()
+	{
+
+
+	}
+
+
+	public void ExitGameButton()
+	{
+		Application.Quit();
+	}
+}
