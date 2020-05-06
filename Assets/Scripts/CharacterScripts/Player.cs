a5bf1350c9b88e742619a62b2aefc1b6450f7f6b
Admin <dimakaza4ka@gmail.com>
2020-05-07 00:01:24 +0300

    Added first animations

--- Assets/Scripts/CharacterScripts/Player.cs
+++ Assets/Scripts/CharacterScripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : AbstractCharacter
 	private Vector3 lastPosition;
 	private Vector3 CheckPointPosition;
 	private bool isDead = false;
+	private bool isFacingRight = true;
 
 	// Start is called before the first frame update
 	void Start()
@@ -33,10 +34,44 @@ public class Player : AbstractCharacter
         powerOfSpell = powerOfWill * 5 + level + charisma / 4;
         mentalDefence = powerOfWill * 2 + level;
     }
-    
 
-    // Update is called once per frame
-    void Update()
+	private void Flip()
+	{
+		//меняем направление движения персонажа
+		isFacingRight = !isFacingRight;
+		//получаем размеры персонажа
+		Vector3 theScale = transform.localScale;
+		//зеркально отражаем персонажа по оси Х
+		theScale.x *= -1;
+		//задаем новый размер персонажа, равный старому, но зеркально отраженный
+		transform.localScale = theScale;
+	}
+	private void FixedUpdate()
+	{
+		//используем Input.GetAxis для оси Х. метод возвращает значение оси в пределах от -1 до 1.
+		//при стандартных настройках проекта 
+		//-1 возвращается при нажатии на клавиатуре стрелки влево (или клавиши А),
+		//1 возвращается при нажатии на клавиатуре стрелки вправо (или клавиши D)
+		float move = Input.GetAxis("Horizontal");
+
+		//в компоненте анимаций изменяем значение параметра Speed на значение оси Х.
+		//приэтом нам нужен модуль значения
+		animator.SetFloat("Speed", Mathf.Abs(move));
+
+		//обращаемся к компоненту персонажа RigidBody2D. задаем ему скорость по оси Х, 
+		//равную значению оси Х умноженное на значение макс. скорости
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+		//если нажали клавишу для перемещения вправо, а персонаж направлен влево
+		if (move > 0 && !isFacingRight)
+			//отражаем персонажа вправо
+			Flip();
+		//обратная ситуация. отражаем персонажа влево
+		else if (move < 0 && isFacingRight)
+			Flip();
+	}
+	// Update is called once per frame
+	void Update()
     {
 		// get the input this frame
 		float vertical = Input.GetAxis("Vertical");
@@ -47,7 +82,7 @@ public class Player : AbstractCharacter
 		{
 			animator.speed = 0.0f;
 		}
-
+			
 		// reset the velocity each frame
 		GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
 
